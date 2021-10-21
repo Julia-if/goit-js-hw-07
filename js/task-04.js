ceca787407b51dddd7e8d69b7618e228f2b91206
Julia-if <juliaif18@gmail.com>
2021-10-21 18:17:52 +0300

    update task -04

--- js/task-04.js
+++ js/task-04.js
@@ -2,15 +2,16 @@ const btnDecrement = document.querySelector('[data-action="decrement"]');
 const btnIncrement = document.querySelector('[data-action="increment"]');
 const startValue = document.querySelector('#value');
 
+
 let currentValue = 0;
-const decrement = () => 
-    btnDecrement.textContent = currentValue -=1;
-const increment = () =>
-    btnIncrement.textContent= currentValue +=1;
+const decrement = (e) => 
+    startValue.textContent = currentValue -= 1;
+
+const increment = () => 
+     startValue.textContent= currentValue +=1;
 
 btnDecrement.addEventListener('click', decrement);
 btnIncrement.addEventListener('click', increment);
-
 
 
 
